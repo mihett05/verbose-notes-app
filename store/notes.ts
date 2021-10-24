96c3cde4908ddd5c75045878a4606f13602a0bdc
mihett05 <mihett05@yandex.ru>
2021-10-24 19:02:18 +0500

    Added store

---
+++ store/notes.ts
@@ -0,0 +1,84 @@
+import { createEvent, createStore } from 'effector';
+
+type Note = {
+  uid: string;
+  name: string;
+  content: string;
+  createdAt: Date;
+};
+
+type NoteAction<T = string> = {
+  uid: string;
+  data: T;
+};
+
+type NotesStore = Note[];
+
+export const $notes = createStore<NotesStore>([]);
+
+export const addNote = createEvent();
+export const editNoteName = createEvent<NoteAction>();
+export const editNoteContent = createEvent<NoteAction>();
+export const deleteNote = createEvent<string>();
+export const saveNotes = createEvent();
+
+const loadNotes = createEvent();
+
+$notes
+  .on(loadNotes, (state) => {
+    const rawLoaded = localStorage.getItem('notes');
+    if (rawLoaded !== null) {
+      try {
+        const loadedJson = JSON.parse(rawLoaded);
+        if (Array.isArray(loadedJson)) {
+          // parse notes from localStorage
+          return (loadedJson.filter((v) => v.uid && v.name && v.content && v.createdAt) as Note[]).map((v) => {
+            const { uid, name, content } = v;
+            return {
+              uid,
+              name,
+              content,
+              createdAt: new Date(v.createdAt),
+            };
+          });
+        }
+      } catch (e) {}
+    }
+
+    return [];
+  })
+  .on(saveNotes, (state) => {
+    localStorage.setItem('notes', JSON.stringify(state));
+    return [...state];
+  })
+  .on(addNote, (state) => {
+    const uid = Math.random().toString(16).substr(2, 8).toUpperCase();
+    const newNote: Note = {
+      uid,
+      name: `Untitled note ${uid}`,
+      content: '',
+      createdAt: new Date(),
+    };
+    return [...state, newNote];
+  })
+  .on(editNoteName, (state, action) => {
+    const result = state.find((v) => v.uid === action.uid);
+    if (result) {
+      result.name = action.data;
+    }
+
+    return [...state];
+  })
+  .on(editNoteContent, (state, action) => {
+    const result = state.find((v) => v.uid === action.uid);
+    if (result) {
+      result.content = action.data;
+    }
+
+    return [...state];
+  })
+  .on(deleteNote, (state, uid) => {
+    return state.filter((v) => v.uid !== uid);
+  });
+
+loadNotes();
---
+++ store/preferences.ts
@@ -0,0 +1,58 @@
+import { createEvent, createStore } from 'effector';
+
+type ColorMode = 'light' | 'dark';
+
+interface PreferencesStore {
+  language: string;
+  colorMode: ColorMode;
+}
+
+export const $preferences = createStore<PreferencesStore>({
+  language: 'en',
+  colorMode: 'light',
+});
+
+export const changeLanguage = createEvent<string>();
+export const toggleColorMode = createEvent();
+const loadPreferences = createEvent();
+
+const getAndSetDefaultLocalStorage = (key: string, defaultValue: string, isOk?: (value: string) => boolean): string => {
+  // get value from localStorage or return defaultValue and set defaultValue to localStorage
+  const value = localStorage.getItem(key);
+  if (value === null || (isOk && !isOk(value))) {
+    localStorage.setItem(key, defaultValue);
+    return defaultValue;
+  }
+
+  return value;
+};
+
+$preferences
+  .on(loadPreferences, (state) => {
+    return {
+      ...state,
+      language: getAndSetDefaultLocalStorage('language', state.language),
+      colorMode: getAndSetDefaultLocalStorage(
+        'colorMode',
+        state.colorMode,
+        (value) => value === 'light' || value === 'dark',
+      ) as ColorMode,
+    };
+  })
+  .on(changeLanguage, (state, language: string) => {
+    localStorage.setItem('language', language);
+    return {
+      ...state,
+      language,
+    };
+  })
+  .on(toggleColorMode, (state) => {
+    const newMode = state.colorMode === 'light' ? 'dark' : 'light';
+    localStorage.setItem('colorMode', newMode);
+    return {
+      ...state,
+      colorMode: newMode,
+    };
+  });
+
+loadPreferences();
